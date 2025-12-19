eb228f0cdbd6aaa2529355109cfbdf7b77c205f8
underthebluemoon <underthebluemoon0@gmail.com>
2025-12-20 01:11:12 +0900

    251219 예약 조회 리스트 css 스타일

--- src/components/reserveList/ReserveList.jsx
+++ src/components/reserveList/ReserveList.jsx
@@ -24,6 +24,113 @@ export default function ReserveList() {
         <div className="reserve-list-title-wrapper page-title-wrapper">
           <h2 className="reserve-list-title">내 보따리 확인하기</h2>
         </div>
+
+        {/* 페이지 내용 컨테이너 */}
+        <div className="reserve-list-body">
+
+          {/* 예약 아이템1 */}
+          <div className="reserve-lsit-content-wrapper">
+
+            {/* 상단 */}
+            <div className="reserve-list-content-header">
+              {/* 보관/배송 타입 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-type-ds reserve-list-type-s">보관</span>
+              </div>
+              {/* 예약/배송/보관/완료/취소 상태 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-status reserve-list-status-before">예약 중</span>
+              </div>
+            </div>
+
+            {/* 중간 */}
+            <div className="reserve-list-content-middle">
+              <div className="reserve-list-location-info">
+                <span>동대구역 보관소</span>
+              </div>
+            </div>
+
+            {/* 하단 */}
+            <div className="reserve-list-content-date">
+              <div className="reserve-list-date-info">
+                <span>2025-12-12 09:00 부터</span>
+                <span>2025-12-12 21:00 까지</span>
+              </div>
+            </div>
+
+          </div>
+
+          {/* 예약 아이템2 */}
+          <div className="reserve-lsit-content-wrapper">
+
+            {/* 상단 */}
+            <div className="reserve-list-content-header">
+              {/* 보관/배송 타입 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-type-ds reserve-list-type-d">배송</span>
+              </div>
+              {/* 예약/배송/보관/완료/취소 상태 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-status reserve-list-status-now">배송 중</span>
+              </div>
+            </div>
+
+            {/* 중간 */}
+            <div className="reserve-list-content-middle">
+              <div className="reserve-list-location-info">
+                <span>수성 호텔 에서</span>
+                <span>동대구역 보관소 까지</span>
+              </div>
+            </div>
+
+            {/* 하단 */}
+            <div className="reserve-list-content-date">
+              <div className="reserve-list-date-info">
+                <span>2025-12-12 09:00 부터</span>
+                <span>2025-12-12 21:00 까지</span>
+              </div>
+            </div>
+
+          </div>
+
+          {/* 예약 아이템2 */}
+          <div className="reserve-lsit-content-wrapper">
+
+            {/* 상단 */}
+            <div className="reserve-list-content-header">
+              {/* 보관/배송 타입 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-type-ds reserve-list-type-d">배송</span>
+              </div>
+              {/* 예약/배송/보관/완료/취소 상태 */}
+              <div className="reserve-list-type-wrapper">
+                <span className="reserve-list-status reserve-list-status-done">완료</span>
+              </div>
+            </div>
+
+            {/* 중간 */}
+            <div className="reserve-list-content-middle">
+              <div className="reserve-list-location-info">
+                <span>수성 호텔 에서</span>
+                <span>동대구역 보관소 까지</span>
+              </div>
+            </div>
+
+            {/* 하단 */}
+            <div className="reserve-list-content-date">
+              <div className="reserve-list-date-info">
+                <span>2025-12-12 09:00 부터</span>
+                <span>2025-12-12 21:00 까지</span>
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+
       </div>
     </>
   )
