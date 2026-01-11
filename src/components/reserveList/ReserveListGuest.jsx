2c23130cce5917448f3bcd19193b20b8f01a8402
underthebluemoon <underthebluemoon0@gmail.com>
2026-01-11 23:18:42 +0900

    260111 비회원예약 페이지, 예약이 없는 경우 대응

--- src/components/reserveList/ReserveListGuest.jsx
+++ src/components/reserveList/ReserveListGuest.jsx
@@ -1,23 +1,42 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./ReserveListGuest.css";
 import ReserveItem from "./ReserveItem";
+import Loading from "../common/Loading.jsx";
 
 export default function ReserveListGuest() {
+  const navigate = useNavigate();
   const reservation = useSelector(state => state.reserve.reservation);
+
+  // PENDING_PAYMENT 제외한 유효한 예약인지 확인
+  const isValidReservation = reservation && reservation.state !== 'PENDING_PAYMENT';
 
   console.log('비회원 예약 정보: ', reservation)
   return(
     <div className="reserve-list-body">
+      {/* 유효한 예약이 없는 경우 */}
       {
-        reservation &&
-        <motion.div
-          initial={{ opacity: 0, y: 15 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <ReserveItem data={reservation}/>
-        </motion.div>
+        !isValidReservation && (
+          <div className="reserve-list-noItem">
+            <Loading fullScreen={false} text="조회된 예약이 없습니다."/>
+            <div className="reserve-list-noItem-btn-wrapper">
+              <button className="reserve-list-noItem-btn" onClick={() => navigate('/reserve')}>예약 하러가기</button>
+            </div>
+          </div>
+        )
+      }
+      {/* 유효한 예약이 있는 경우 */}
+      {
+        isValidReservation && (
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <ReserveItem data={reservation}/>
+          </motion.div>
+        )
       }
     </div>
   )
