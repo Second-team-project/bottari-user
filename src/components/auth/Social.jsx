83f3f259b7b11ab2cfdf22106809d7625966d13c
underthebluemoon <underthebluemoon0@gmail.com>
2026-01-11 23:11:18 +0900

    260111 차단 유저 리다이렉트 처리, 로그인 중 로그인 컴포넌트 출력

--- src/components/auth/Social.jsx
+++ src/components/auth/Social.jsx
@@ -1,14 +1,30 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 import { reissueThunk } from "../../store/thunks/authThunk.js";
+import { toast, Toaster } from "sonner";
+import Loading from "../common/Loading.jsx";
 
 export default function Social() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const errorMsg = searchParams.get('error');
 
   useEffect(() => {
+    // 에러메세지 쿼리가 있으면 쫓아냄
+    if (errorMsg) {
+      toast.error(decodeURIComponent(errorMsg));
+
+      const timer = setTimeout(() => {
+        navigate('/', { replace: true });
+      }, 1000)
+      // reissueThunk 실행 안 함
+      return () => clearTimeout(timer); 
+    }
+
+    // 에러가 없으면 정상 로그인 진행
     async function getAuth() {
       try {
         const result = await dispatch(reissueThunk()).unwrap();
@@ -19,16 +35,17 @@ export default function Social() {
 
       } catch (error) {
         console.log('Social: ', error);
-        alert('소셜 로그인 실패');
+        toast.error('소셜 로그인 실패');
         navigate('/login', { replace: true });
       }
     }
     getAuth();
-  }, []);
+  }, [errorMsg]);
 
   return (
     <>
-      <p>로그인 중</p>
+      <Toaster position="top-center" />
+      <Loading text="로그인 중..." />
     </>
   )
 }
